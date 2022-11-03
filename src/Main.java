09a0d5053ab0a3b4e55fb2b53dce2870fb4b258b
Vyrvins <olya.vyrvinskaya@gmail.com>
2022-11-03 20:04:50 +0300

    reformat code

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
         int price = 10_000; // цена билета
         int amount = 20; // кол-во рублей для 1 бонусной мили
-        int bonus = price/amount;
+
+        int bonus = price / amount;
+
         System.out.println(bonus);
     }
 }
